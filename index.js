afd32a37f0dba68ae4b13122552479432c088267
adesug <adesugiantoro89@gmail.com>
2021-07-16 06:47:55 +0700

    cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config({});
 const express = require('express');
+var cors = require('cors')
 const mainRoutes = require('./src/routes');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
